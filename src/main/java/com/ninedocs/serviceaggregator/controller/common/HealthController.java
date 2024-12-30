dcf790ce66c48975e3195cceadbb2955e4897837
yerimkim <kiel0103@naver.com>
2024-12-30 18:38:33 +0900

    NINEDOCS-15 Swagger 적용

--- src/main/java/com/ninedocs/serviceaggregator/controller/common/HealthController.java
+++ src/main/java/com/ninedocs/serviceaggregator/controller/common/HealthController.java
@@ -1,14 +1,16 @@
 package com.ninedocs.serviceaggregator.controller.common;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Tag(name = "System")
 @RestController
 public class HealthController {
 
-  @GetMapping("/api/v1/health")
-  public Mono<String> getTest() {
+  @GetMapping("/api/v1/system/health")
+  public Mono<String> healthCheck() {
     return Mono.just("Healthy");
   }
 }
