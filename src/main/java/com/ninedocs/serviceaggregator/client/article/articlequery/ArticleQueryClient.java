2827864b59d0ce4214a353575092d5756e97c4f0
Kim Yerim <kiel0103@naver.com>
2025-01-17 10:24:56 +0900

    NINEDOCS-119 문제 페이지 문제 조회 API (#15)
    * docs: README.md swagger uri 최신화
    
    * feat: article 조회 API

---
+++ src/main/java/com/ninedocs/serviceaggregator/client/article/articlequery/ArticleQueryClient.java
@@ -0,0 +1,39 @@
+package com.ninedocs.serviceaggregator.client.article.articlequery;
+
+import com.ninedocs.serviceaggregator.client.article.articlequery.dto.ArticleQueryResponse;
+import com.ninedocs.serviceaggregator.client.common.dto.DomainResponse;
+import com.ninedocs.serviceaggregator.client.common.error.Unknown2xxErrorException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleQueryClient {
+
+  private static final String DOMAIN_NAME = "Article";
+
+  private final WebClient articleWebClient;
+
+  public Mono<ArticleQueryResponse> getArticle(Long articleId) {
+    final String uriPath = "/api/v1/article/article/" + articleId + "/";
+
+    return articleWebClient.get()
+        .uri(uriPath)
+        .accept(MediaType.APPLICATION_JSON)
+        .retrieve()
+        .bodyToMono(new ParameterizedTypeReference<DomainResponse<ArticleQueryResponse>>() {
+        })
+        .flatMap(domainResponse -> {
+          if (!domainResponse.getSuccess()) {
+            return Mono.error(
+                new Unknown2xxErrorException(DOMAIN_NAME, uriPath, domainResponse.getErrorCode())
+            );
+          }
+          return Mono.just(domainResponse.getData());
+        });
+  }
+}
---
+++ src/main/java/com/ninedocs/serviceaggregator/controller/article/ArticleController.java
@@ -0,0 +1,29 @@
+package com.ninedocs.serviceaggregator.controller.article;
+
+import com.ninedocs.serviceaggregator.client.article.articlequery.ArticleQueryClient;
+import com.ninedocs.serviceaggregator.controller.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@Tag(name = "문제페이지")
+@RestController
+@RequiredArgsConstructor
+public class ArticleController {
+
+  private final ArticleQueryClient articleQueryClient;
+
+  @GetMapping("/api/v1/article/{articleId}")
+  public Mono<ResponseEntity<ApiResponse<String>>> getArticle(
+      @PathVariable("articleId") Long articleId
+  ) {
+    return articleQueryClient.getArticle(articleId)
+        .map(article -> ResponseEntity.ok(ApiResponse.success(
+            article.getContents()
+        )));
+  }
+}
