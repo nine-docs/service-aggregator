d3849801ea480e0c56c617a89d47641129f811f6
yelimkim98 <kiel0103@naver.com>
2025-01-27 20:31:35 +0900

    feat: CORS 설정 추가

---
+++ src/main/java/com/ninedocs/serviceaggregator/filter/CorsFilter.java
@@ -0,0 +1,26 @@
+package com.ninedocs.serviceaggregator.filter;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Component
+public class CorsFilter {
+
+  @Bean
+  public CorsWebFilter corsWebFilter() {
+    CorsConfiguration corsConfig = new CorsConfiguration();
+    corsConfig.setAllowCredentials(true);
+    corsConfig.addAllowedHeader("*");
+    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfig);
+
+    return new CorsWebFilter(source);
+  }
+}
