ad8914a690365a4f49e28d54f8e47e9757c81a0b
Kim Yerim <kiel0103@naver.com>
2025-02-10 16:20:01 +0900

    NINEDOCS-205 북마크 생성 시 500 에러 (#29)

--- src/main/java/com/ninedocs/serviceaggregator/client/subcontents/bookmark/dto/BookmarkCreateClientResponse.java
+++ src/main/java/com/ninedocs/serviceaggregator/client/subcontents/bookmark/dto/BookmarkCreateClientResponse.java
@@ -1,9 +1,15 @@
 package com.ninedocs.serviceaggregator.client.subcontents.bookmark.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BookmarkCreateClientResponse {
 
-  private Long id;
+  private Long bookmarkId;
+  private Long userId;
+  private Long articleId;
+  private LocalDateTime createdAt;
 }
